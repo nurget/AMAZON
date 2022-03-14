f50bc3fc5ab76e42e957d7c6fae38bd1adb76041
Ayeong Kim <devayeong@gmail.com>
2022-03-14 18:05:11 +0900

    firebase 연동

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDBzi65x0hbcjRB1IcnwSgLoPUEU0hZxYE",
+    authDomain: "amzn-nurget.firebaseapp.com",
+    projectId: "amzn-nurget",
+    storageBucket: "amzn-nurget.appspot.com",
+    messagingSenderId: "880232838437",
+    appId: "1:880232838437:web:fc43af19214a57c72d16cc"
+  };
